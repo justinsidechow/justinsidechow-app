f6234ae7c26b7e5a74dd008fd8a538bc0873c474
justinsidechow <justinsidechow@gmail.com>
2021-05-30 02:45:36 -0700

    'finished-some-nav-bar-stuff'

---
+++ src/components/Button.tsx
@@ -0,0 +1,8 @@
+function Button () {
+    return (
+        '123'
+    )
+
+}
+
+export default Button;
---
+++ src/components/Navbar/MenuItems.tsx
@@ -0,0 +1,29 @@
+const MenuItems = [
+  {
+    title: "Home",
+    url: "",
+    cName: "nav-links", //cName == class name
+  },
+  {
+    title: "Home",
+    url: "",
+    cName: "nav-links", //cName == class name
+  },
+  {
+    title: "Home",
+    url: "",
+    cName: "nav-links", //cName == class name
+  },
+  {
+    title: "Home",
+    url: "",
+    cName: "nav-links", //cName == class name
+  },
+  {
+    title: "Home",
+    url: "",
+    cName: "nav-links-mobile", //cName == class name
+  },
+];
+
+export default MenuItems;
