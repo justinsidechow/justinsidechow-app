7279e87882534e0ee66295215d12d7993e7cdd9f
justinsidechow <justinsidechow@gmail.com>
2021-06-27 06:22:33 -0700

    actually-fixed-nav-menu-animation-so-now-complete

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,11 +7,41 @@ import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 class Navbar extends Component {
   state = {
     clicked: false,
+    firstRender: false,
+    width: 0,
+    height: 0,
   };
 
   handleClick = () => {
     this.setState({ clicked: !this.state.clicked });
   };
+
+  navMenuInitiation() {
+    if (this.state.width >= 960) {
+      this.state.clicked = false;
+      this.state.firstRender = false;
+      return "nav-menu firstactive";
+    }
+
+    if (this.state.clicked) {
+      this.state.firstRender = true;
+      return "nav-menu active";
+    } else if (!this.state.clicked && this.state.firstRender) {
+      return "nav-menu inactive";
+    } else {
+      return "nav-menu firstactive";
+    }
+  }
+
+  updateDimensions = () => {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  };
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
 
   render() {
     return (
@@ -25,11 +55,7 @@ class Navbar extends Component {
             className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
           ></i>
         </div>
-        <ul
-          className={
-            this.state.clicked ? "nav-menu active" : "nav-menu inactive"
-          }
-        >
+        <ul className={this.navMenuInitiation()}>
           {MenuItems.map((item, index) => {
             return (
               <li key={index}>
