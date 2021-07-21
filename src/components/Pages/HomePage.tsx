092b075e81872174c7757c829cefeaa347bc7080
justinsidechow <justinsidechow@gmail.com>
2021-07-20 19:52:54 -0700

    fixed-div-space-and-background-color

--- src/components/Pages/HomePage.tsx
+++ src/components/Pages/HomePage.tsx
@@ -1,22 +1,28 @@
 import "./Page.css";
+import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div className="container">
-      <img
-        className="home-page-main-image"
-        src={
-          process.env.PUBLIC_URL + "/Photo/place-holder-home-page-cropped.JPG"
-        }
-      ></img>
-      <div className="home-page-mission">
-        Programming the future <br></br>
-        One line at a time <br></br>
-        <a href="/about">
-          <button className="learn-more-button">Learn More Here</button>
-        </a>
+    <div>
+      <div className="container">
+        <img
+          className="home-page-main-image"
+          src={process.env.PUBLIC_URL + "/Photo/place-holder-home-page.JPG"}
+        ></img>
+        <div className="home-page-mission">
+          Programming the future <br></br>
+          One line at a time <br></br>
+          <Link className="learn-more-button" to="/about">
+            Learn More Here
+          </Link>
+        </div>
       </div>
-      <div>this is an example</div>
+      {/*
+      <img
+        className="page-background-image"
+        src={process.env.PUBLIC_URL + "/Photo/computer_background.JPG"}
+      ></img>
+       */}
     </div>
   );
 };
