46f358b3c7b3ec82ec35dde352a55da848142d65
justinsidechow <justinsidechow@gmail.com>
2021-08-04 14:08:00 -0700

    started-contact-page-with-email-form

--- src/components/Pages/ContactPage.tsx
+++ src/components/Pages/ContactPage.tsx
@@ -1,7 +1,24 @@
 const ContactPage = () => {
   return (
-    <div>
-      <h3>Contact Page</h3>
+    <div className="container">
+      <div className="page-heading">
+        <div className="page-title">Contact</div>
+        <div className="page-mission">Connecting the Nodes</div>
+      </div>
+      <div className="column-section">
+        <div className="column-center-contact">
+          <form className="column-contact" id="contact-form">
+            <input type="text" name="user_name" placeholder="Name" />
+            <input type="email" name="user_email" placeholder="Email" />
+            <textarea name="message" placeholder="Message" />
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+      </div>
+      <img
+        className="main-image"
+        src={process.env.PUBLIC_URL + "/Photo/contact-background.JPG"}
+      ></img>
     </div>
   );
 };
